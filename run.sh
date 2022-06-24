ce563e0c6b97f5fba117538267cc362bf98e2f14
zoharli <zoharg@outlook.com>
2022-06-24 05:39:17 -0500

    updated

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #! /usr/bin/zsh
 
 
- for file in `ls $HOME/detectron2/data`
+ for file in `ls ./data`
  do
      if [ "${file: -4}"x = ".mp4"x ];then
-         echo "===============>processing $file"
+        echo "===============>processing $file"
 #         python infer.py anchor-based --ckpt-path ../models/pretrain_ab_basic/checkpoint/summe.yml.4.pt --source ../../detectron2/data/$file --save-path ../result/SummeResult_"$file"
-         echo python infer.py anchor-based --ckpt-path ../models/pretrain_ab_basic/checkpoint/tvsum.yml.4.pt --source ../../detectron2/data/$file --save-path ../result/1SegResult_"$file"
-         python infer.py anchor-based --ckpt-path ../models/pretrain_ab_basic/checkpoint/tvsum.yml.4.pt --source ../../detectron2/data/$file --save-path ../result/1SegResult_"$file"
+        echo python main.py feature_type=vggish video_paths="[./data/$file]" on_extraction=save_numpy
+        python main.py feature_type=vggish video_paths="[./data/$file]" on_extraction=save_numpy
      fi
  done
+ 
+
+
